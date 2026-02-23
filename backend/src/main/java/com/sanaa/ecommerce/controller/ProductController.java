4c0daf9ccffffc5d65073cc2fa919ac284dcfcc0
Your Name <you@example.com>
2026-02-23 16:17:58 +0530

    Backend updates for version 2 ( added ResponseEntity )

--- backend/src/main/java/com/sanaa/ecommerce/controller/ProductController.java
+++ backend/src/main/java/com/sanaa/ecommerce/controller/ProductController.java
@@ -4,10 +4,10 @@ package com.sanaa.ecommerce.controller;
 import com.sanaa.ecommerce.model.Product;
 import com.sanaa.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +28,18 @@ public class ProductController {
     public List<Product> getProduct(){
         return service.getProducts();
     }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+
+        Product product=service.getProductById(id);
+        if(product!=null)
+            return new ResponseEntity<>(product,HttpStatus.OK);
+
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND) ;
+    }
+
 
 
 }
